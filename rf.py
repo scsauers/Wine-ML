f14187ff937ace881d8732d1792ac3e4fe5d56f3
SeanL77 <153564088+SeanL77@users.noreply.github.com>
2023-12-16 10:38:55 -0600

    Add files via upload

---
+++ rf.py
@@ -0,0 +1,36 @@
+# Import necessary libraries
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+
+# Load the wine dataset
+file_path = r"C:\Users\sluca\Downloads\Wine Data.xlsx"
+wine_data = pd.read_excel(file_path)
+
+# Separate features (X) and target variable (y)
+X = wine_data.drop('quality', axis=1)
+y = wine_data['quality']
+
+# Split the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Initialize the Random Forest Classifier
+rf_classifier = RandomForestClassifier(n_estimators=100, random_state=42)
+
+# Train the classifier on the training data
+rf_classifier.fit(X_train, y_train)
+
+# Make predictions on the test data
+y_pred = rf_classifier.predict(X_test)
+
+# Evaluate the performance of the classifier
+accuracy = accuracy_score(y_test, y_pred)
+conf_matrix = confusion_matrix(y_test, y_pred)
+class_report = classification_report(y_test, y_pred)
+
+# Print the results
+print(f'Accuracy: {accuracy:.4f}\n')
+print(f'Confusion Matrix:\n{conf_matrix}\n')
+print(f'Classification Report:\n{class_report}')
+
